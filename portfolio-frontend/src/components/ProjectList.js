98c5af88d096ac58ba00ada51a5763ea22c2596a
Cholo1393 <jean-christophe.matti@epitech.eu>
2024-11-03 23:29:51 +0100

    portfolio maj

--- portfolio-frontend/src/components/ProjectList.js
+++ portfolio-frontend/src/components/ProjectList.js
@@ -1,7 +1,6 @@
 // src/components/ProjectList.js
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'; // Ajoutez cette ligne pour importer useState et useEffect
 import { getProjects } from '../services/api';
-import { Link } from 'react-router-dom'; // Ajoutez cette ligne pour utiliser le Link
 
 const ProjectList = () => {
     const [projects, setProjects] = useState([]);
@@ -20,17 +19,14 @@ const ProjectList = () => {
     }, []);
 
     return (
-        <div className="container mx-auto p-4">
-            <h2 className="text-2xl font-bold text-center mb-6">Mes Projets</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                {projects.map(project => (
-                    <Link key={project._id} to={`/projects/${project._id}`} className="bg-white shadow-md rounded-lg p-4 hover:shadow-lg transition">
-                        <h3 className="text-xl font-semibold text-primary">{project.title}</h3>
-                        <p className="text-gray-700">{project.description}</p>
-                        <a href={project.githubLink} className="text-blue-500 hover:underline">GitHub</a>
-                    </Link>
-                ))}
-            </div>
+        <div>
+            {projects.map(project => (
+                <div key={project._id}>
+                    <h2>{project.title}</h2>
+                    <p>{project.description}</p>
+                    <a href={project.githubLink}>GitHub</a>
+                </div>
+            ))}
         </div>
     );
 };
