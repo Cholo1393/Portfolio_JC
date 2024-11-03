7c7572715835efdfbf89bfff5ea6603b18c6eeaa
Cholo1393 <jean-christophe.matti@epitech.eu>
2024-11-03 23:04:51 +0100

    portfolio maj

--- portfolio-frontend/src/services/api.js
+++ portfolio-frontend/src/services/api.js
@@ -1,47 +1,50 @@
+// src/services/api.js
+
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://portfolio-backend-jc.fly.dev/api',
+    baseURL: 'https://portfolio-backend-jc.fly.dev/api',
 });
 
-// Ajouter un intercepteur pour inclure le token d'authentification dans chaque requête
+// Intercepteur pour inclure le token d'authentification
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
 }, (error) => {
-  return Promise.reject(error);
+    return Promise.reject(error);
 });
 
 // Fonctions d'API
 export const registerUser = async (userData) => {
-  return await api.post('/auth/register', userData);
+    return await api.post('/auth/register', userData);
 };
 
 export const loginUser = async (userData) => {
-  return await api.post('/auth/login', userData);
+    return await api.post('/auth/login', userData);
 };
 
 export const postComment = async (projectId, commentData) => {
-  return await api.post(`/comments/${projectId}`, commentData);
+    return await api.post(`/comments/${projectId}`, commentData);
 };
 
 export const getComments = async (projectId) => {
-  return await api.get(`/comments/${projectId}`);
+    return await api.get(`/comments/${projectId}`);
 };
 
 export const getProjects = async () => {
-  return await api.get('/projects');
+    return await api.get('/projects');
+};
+
+// ** Ajoutez la fonction getProject ici **
+export const getProject = async (projectId) => {
+    return await api.get(`/projects/${projectId}`);
 };
 
 export const contactForm = async (contactData) => {
-  return await api.post('/contact', contactData);
+    return await api.post('/contact', contactData);
 };
-// src/services/api.js
-// export const getProject = async (projectId) => {
-//   return await api.get(`/projects/${projectId}`); // Mettez à jour l'URL avec votre backend
-// };
 
 export default api;
