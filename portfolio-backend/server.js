b97888adba26d6e4702e94b3fbb1b7063eccc1a7
Cholo1393 <jean-christophe.matti@epitech.eu>
2024-11-03 19:35:00 +0100

    portfolio maj

--- portfolio-backend/server.js
+++ portfolio-backend/server.js
@@ -1,7 +1,8 @@
+// server.js
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const db = require('./config/db'); // Assurez-vous que ce chemin est correct
+const db = require('./config/db');
 const projectRoutes = require('./routes/projectRoutes');
 const commentRoutes = require('./routes/commentRoutes');
 const authRoutes = require('./routes/authRoutes');
@@ -10,13 +11,19 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware pour CORS
-app.use(cors());
+const corsOptions = {
+    origin: 'https://mattidev.netlify.app', // URL de votre frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 // Middleware pour traiter le JSON
 app.use(express.json());
 
 // Connexion à la base de données
-db(); // Connexion à MongoDB
+db();
 
 // Routes
 app.use('/api/projects', projectRoutes);
--- portfolio-frontend/src/services/api.js
+++ portfolio-frontend/src/services/api.js
@@ -1,15 +1,30 @@
-// services/api.js
+// src/services/api.js
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL, // Utilise l'URL définie dans .env
+  baseURL: 'https://portfolio-backend-jc.fly.dev/api', // URL de votre backend déployé
 });
 
-export const registerUser = (data) => api.post('/register', data);
-export const loginUser = (data) => api.post('/login', data);
-export const postComment = (projectId, data) => api.post(`/projects/${projectId}/comments`, data);
-export const getComments = (projectId) => api.get(`/projects/${projectId}/comments`);
-export const getProjects = () => api.get('/projects');
-export const contactForm = (data) => api.post('/contact', data);
+export const registerUser = async (userData) => {
+  return await api.post('/auth/register', userData);
+};
 
-export default api;
+export const loginUser = async (userData) => {
+  return await api.post('/auth/login', userData);
+};
+
+export const postComment = async (projectId, commentData) => {
+  return await api.post(`/comments/${projectId}`, commentData);
+};
+
+export const getComments = async (projectId) => {
+  return await api.get(`/comments/${projectId}`);
+};
+
+export const getProjects = async () => {
+  return await api.get('/projects');
+};
+
+export const contactForm = async (contactData) => {
+  return await api.post('/contact', contactData);
+};
