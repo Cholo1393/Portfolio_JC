635c9ebf7ff60d45542be6bd6fc829338d0655b6
Cholo1393 <jean-christophe.matti@epitech.eu>
2024-11-04 00:26:15 +0100

    portfolio maj

--- portfolio-frontend/src/components/Navbar.js
+++ portfolio-frontend/src/components/Navbar.js
@@ -1,17 +1,16 @@
-// src/components/Navbar.js
+// Navbar.js
 import React from 'react';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-container">
-        <div className="navbar-brand">MattiDev</div>
-        <div className="navbar-links">
-          <a href="#home">Accueil</a>
-          <a href="#about">À Propos</a>
-          <a href="#projects">Projets</a>
-          <a href="#contact">Contact</a>
-        </div>
+      <div className="navbar-brand">
+        <h1 style={{ margin: 0 }}>Mon Portfolio</h1>
+      </div>
+      <div className="navbar-links">
+        <a href="#about">À Propos</a>
+        <a href="#projects">Projets</a>
+        <a href="#contact">Contact</a>
       </div>
     </nav>
   );
