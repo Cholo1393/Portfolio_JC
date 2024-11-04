41662b113ed35c360b11334c1d360ef95ca5ce83
Cholo1393 <jean-christophe.matti@epitech.eu>
2024-11-04 06:33:54 +0100

    portfolio maj

--- portfolio-frontend/src/components/Home.js
+++ portfolio-frontend/src/components/Home.js
@@ -1,8 +1,8 @@
 // src/components/Home.js
 const Home = () => {
   return (
-    <section id="home" className="flex items-center justify-center h-screen bg-cover bg-center" style={{ backgroundImage: 'url(/path/to/your/image.jpg)' }}>
-      <h1 className="text-4xl text-white font-bold">Bienvenue sur mon Portfolio</h1>
+    <section id="home" className="flex items-center justify-center h-screen bg-gray-800">
+      <h1 className="text-4xl text-white font-bold animate-bounce">MattiDev</h1>
     </section>
   );
 };
--- portfolio-frontend/tailwind.config.js
+++ portfolio-frontend/tailwind.config.js
@@ -8,3 +8,20 @@ module.exports = {
   },
   plugins: [],
 }
+module.exports = {
+  theme: {
+    extend: {
+      animation: {
+        bounce: 'bounce 1s infinite', // Vous pouvez ajuster la durée et le type d'animation
+      },
+      keyframes: {
+        bounce: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-15%)' }, // Ajustez le pourcentage pour changer la hauteur du rebond
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
