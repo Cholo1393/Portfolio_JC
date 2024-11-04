750d396e9517fcea91a0687be015fc6e98a3da0e
Cholo1393 <jean-christophe.matti@epitech.eu>
2024-11-04 05:56:43 +0100

    portfolio maj

--- portfolio-frontend/src/App.js
+++ portfolio-frontend/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import Navbar from './components/Sidebar';
+import React, { useState } from 'react';
+import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import ProjectList from './components/ProjectList';
@@ -11,18 +11,30 @@ import Sidebar from './components/Sidebar'; // Importer la sidebar
 import './App.css'; // Importer les styles globaux
 
 const App = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <div className="App flex">
-      <Sidebar />
-      <div className="flex-1 ml-64"> {/* Ajuster la marge pour la sidebar */}
-        <Navbar />
-        <main className="main-content">
-          <div id="home"><Home /></div>
-          <div id="about"><About /></div>
-          <div id="projects"><ProjectList /><Project /></div>
-          <div id="contact"><ContactForm /></div>
-          <ProjectComments />
+    <div className="App flex flex-col lg:flex-row">
+      {/* Sidebar avec comportement responsive */}
+      <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+      
+      {/* Contenu principal */}
+      <div className={`flex-1 transition-all ${isSidebarOpen ? 'ml-64' : 'ml-0'} lg:ml-64`}>
+        {/* Navbar avec bouton pour ouvrir/fermer la sidebar en mobile */}
+        <Navbar toggleSidebar={toggleSidebar} />
+        
+        <main className="main-content p-4">
+          <section id="home"><Home /></section>
+          <section id="about" className="mt-8"><About /></section>
+          <section id="projects" className="mt-8"><ProjectList /><Project /></section>
+          <section id="contact" className="mt-8"><ContactForm /></section>
+          <section id="comments" className="mt-8"><ProjectComments /></section>
         </main>
+        
         <Footer />
       </div>
     </div>
