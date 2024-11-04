f9f43f7b702f3bb86b3911c5dda24ef3f3d89672
Cholo1393 <jean-christophe.matti@epitech.eu>
2024-11-04 01:06:39 +0100

    portfolio maj

--- portfolio-frontend/src/App.js
+++ portfolio-frontend/src/App.js
@@ -13,11 +13,12 @@ import './App.css'; // Importer les styles globaux
 const App = () => {
   return (
     <div className="App">
-      <AnimatedSideNav />
+      <Navbar/>
       <div id="home" className="h-screen bg-blue-500 flex items-center justify-center text-white text-3xl">Accueil</div>
       <div id="about" className="h-screen bg-green-500 flex items-center justify-center text-white text-3xl">À Propos</div>
       <div id="projects" className="h-screen bg-red-500 flex items-center justify-center text-white text-3xl">Projets</div>
       <div id="contact" className="h-screen bg-yellow-500 flex items-center justify-center text-white text-3xl">Contact</div>
+      
       <main className="main-content"> {/* Contenu principal avec padding pour éviter d'être masqué par la navbar */}
         <Home />
         <About />
