8be2035ffb3bb7fa4ef773ef536dd46c55c9cdf6
Cholo1393 <jean-christophe.matti@epitech.eu>
2024-11-04 01:54:26 +0100

    portfolio maj

--- portfolio-frontend/src/components/ProjectList.js
+++ portfolio-frontend/src/components/ProjectList.js
@@ -1,30 +1,23 @@
+// src/components/ProjectList.js
+
 import React, { useState, useEffect } from 'react';
 import { getProjects } from '../services/api';
 
 const ProjectList = () => {
     const [projects, setProjects] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
 
     useEffect(() => {
         const fetchProjects = async () => {
             try {
                 const response = await getProjects();
                 setProjects(response.data);
-                setError(null);
             } catch (error) {
-                setError("Erreur lors de la récupération des projets");
                 console.error("Erreur lors de la récupération des projets:", error);
-            } finally {
-                setLoading(false);
             }
         };
 
         fetchProjects();
     }, []);
-
-    if (loading) return <p>Chargement des projets...</p>;
-    if (error) return <p>{error}</p>;
 
     return (
         <div className="project-list">
@@ -34,10 +27,17 @@ const ProjectList = () => {
                     <p>{project.description}</p>
                     <div className="project-images">
                         {project.imageUrls.map((url, index) => (
-                            <img key={index} src={url} alt={`${project.title} image ${index + 1}`} className="project-image" />
+                            <img 
+                                key={index} 
+                                src={url} 
+                                alt={`${project.title} screenshot ${index + 1}`} 
+                                className="project-image" 
+                            />
                         ))}
                     </div>
-                    <a href={project.githubLink} target="_blank" rel="noopener noreferrer">Voir sur GitHub</a>
+                    <a href={project.githubLink} target="_blank" rel="noopener noreferrer">
+                        Voir sur GitHub
+                    </a>
                 </div>
             ))}
         </div>
